c450422c0319a34a0c0b39b7a21e63f2de8b88a9
Cooper Li <43657609+CooperLii@users.noreply.github.com>
2022-06-10 16:04:33 -0500

    Update CarbonFootprint.h

--- CarbonFootprint/CarbonFootprint.h
+++ CarbonFootprint/CarbonFootprint.h
@@ -1,8 +1,4 @@
 /**
- * @author MingFang Li
- * Course: CSCI 333 - Programming Languages
- * Date: 03/01/2019
- * Assignment: 1
  * Project/Class Description:
  * This program calculates the carbon footprint of bicycle, car and bilding.
  * Known bugs: none
